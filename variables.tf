5448b465154dac72637ccded7b84aa745b2e6680
Yoon Lee <yoonlee@kiwishare.com>
2019-08-05 10:04:30 -0500

    Update descriptions

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "domain" {
-  description = "Domain in which to create Route53 record"
+  description = "(Required) Domain in which to create Route53 record"
 }
 
 variable "hostname" {
-  description = "Hostname for Route53 record"
+  description = "(Required) Hostname for Route53 record"
 }
 
 # For cloudfront zone_id is Z2FDTNDATAQYW2
 # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-route53-aliastarget.html#cfn-route53-aliastarget-hostedzoneid
 
 variable "zone_id" {
-  description = "Hostname for Route53 record"
+  description = "(Required) Hostname for Route53 record"
   default     = "Z2FDTNDATAQYW2"
 }
 
